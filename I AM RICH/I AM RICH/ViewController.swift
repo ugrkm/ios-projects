5b67e1c7a41ce533e8a2472d374aeac0b32a1a24
gorkemkrdmn <43235918+gorkemkrdmn@users.noreply.github.com>
2019-07-28 19:33:39 +0300

    I AM RICH uploaded

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Umur Görkem Karaduman on 29.01.2019.
+//  Copyright © 2019 Umur Görkem Karaduman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
